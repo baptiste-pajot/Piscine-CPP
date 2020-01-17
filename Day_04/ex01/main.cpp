00720c83ece2a71df126dbf3559a0ae460ac64e3
baptiste-pajot <bpajot@student.42.fr>
2020-01-17 14:17:24 +0100

    begin ex01

---
+++ Day_04/ex01/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.cpp                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: bpajot <bpajot@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2020/01/17 14:13:56 by bpajot       #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/17 14:15:40 by bpajot      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "Character.hpp"
+#include "RadScorpion.hpp"
+#include "PlasmaRifle.hpp"
+#include "PowerFist.hpp"
+
+int main()
+{
+	Character* zaz = new Character(``zaz'');
+	std::cout << *zaz;
+	Enemy* b = new RadScorpion();
+	AWeapon* pr = new PlasmaRifle();
+	AWeapon* pf = new PowerFist();
+	zaz->equip(pr);
+	std::cout << *zaz;
+	zaz->equip(pf);
+	zaz->attack(b);
+	std::cout << *zaz;
+	zaz->equip(pr);
+	std::cout << *zaz;
+	zaz->attack(b);
+	std::cout << *zaz;
+	zaz->attack(b);
+	std::cout << *zaz;
+	return 0;
+}
